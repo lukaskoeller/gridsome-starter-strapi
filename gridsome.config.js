3ead188731d096bce27fab7581f5d611e07290ef
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-02 23:02:56 +0200

    Edting site name (meta data)

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Gridsome-Starter-Strapi',
   plugins: [
     {
       use: '@gridsome/source-strapi',
