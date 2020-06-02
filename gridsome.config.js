1e95017e780a3e338c82efd598b03bcaa2804e6b
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-02 21:32:58 +0200

    Adding @gridsome/source-strapi to config

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,20 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  plugins: [
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['service'],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        loginData: {
+          identifier: '',
+          password: ''
+        }
+      }
+    }
+  ]
 }
