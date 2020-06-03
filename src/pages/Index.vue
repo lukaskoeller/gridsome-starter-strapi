92fecb20f3430b46d3126575538891f614473329
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-03 02:07:59 +0200

    Show all posts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,92 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <h1>Hi, and welcome to my Blog! 👋</h1>
+    <section>
+      <div class="post" v-for="post in $page.allStrapiPost.edges" :key="post.node.id">
+        <g-image class="post__img" :src="`http://localhost:1337${post.node.picture.formats.small.url}`"/>
+        <g-link :to="`/blog/${post.node.id}`" class="post__info">
+          <h2 class="post__title">{{ post.node.title }}</h2>
+          <h4 class="post__subtitle">{{ post.node.subtitle }}</h4>
+        </g-link>
+      </div>
+    </section>
   </Layout>
 </template>
+
+<page-query>
+query {
+  allStrapiPost {
+    edges {
+      node {
+          id
+          title
+          subtitle
+          article
+          picture {
+            formats {
+              small {
+                url
+              }
+            }
+            caption
+          }
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Gridsome-Starter-Strapi Blog'
   }
 }
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+h1 {
+  margin-bottom: 2rem;
+  margin-top: 0rem;
+  color: var(--color-accent);
+}
+
+section {
+  width: 100%;
+  margin-bottom: 5rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr)); 
+  grid-gap: 2rem;
+}
+
+.post {
+  overflow: hidden;
+  border-radius: 16px;
+  box-shadow: 0px 8px 60px 0px hsla(var(--color-numbers), 0.15);
+}
+
+.post__img {
+  display: block;
+  width: 100%;
+  min-height: 260px;
+  height: 14vw;
+  object-fit: cover;
+}
+
+.post__info {
+  display: block;
+  padding: 1rem;
+}
+
+.post__title {
+  margin: 0;
+  color: var(--color);
+}
+
+.post__subtitle {
+  font-weight: normal;
+  color: var(--color-accent);
+  margin-top: 0.5rem;
+  margin-bottom: 0;
 }
 </style>
