2735bbfee15b4c85c1bbd7a005da19ad7a40ffd9
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-03 02:03:52 +0200

    Adjusting source plugin for post content type

--- gridsome.config.js
+++ gridsome.config.js
@@ -12,7 +12,7 @@ module.exports = {
       options: {
         apiURL: 'http://localhost:1337',
         queryLimit: 1000, // Defaults to 100
-        contentTypes: ['service'],
+        contentTypes: ['post'],
         // Possibility to login with a Strapi user,
         // when content types are not publicly available (optional).
         loginData: {
