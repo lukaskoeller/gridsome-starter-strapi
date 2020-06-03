bf6f9359f39cc5734e5cb65a826eed7509e64bfc
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-03 02:08:15 +0200

    Add template post, which display each blog entry

---
+++ src/templates/Post.vue
@@ -0,0 +1,67 @@
+<template>
+  <Layout>
+    <article>
+        <div class="article__hero">
+            <g-image class="article__img" :src="`http://localhost:1337${$page.strapiPost.picture.url}`"/>
+        </div>
+        <h1 class="article__title">{{ $page.strapiPost.title }}</h1>
+        <div class="article__body">
+            {{ $page.strapiPost.article }}
+        </div>
+    </article>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  strapiPost(id: $id) {
+    title
+    article
+    picture {
+        url
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+article {
+    margin: 0 auto;
+    max-width: 1200px;
+    width: auto;
+    color: var(--color);
+}
+
+.article__hero {
+    width: 100%;
+    height: 500px;
+}
+
+.article__img {
+    width: 100%;
+    height: 100%;
+    border-radius: 16px;
+    object-fit: cover;
+}
+
+.article__title {
+    font-weight: normal;
+    font-size: 2rem;
+    margin-top: 2rem;
+    margin-bottom: 0rem;
+}
+
+.article__body {
+    font-size: 1.1rem;
+    line-height: var(--line-height);
+    white-space: pre-line;
+}
+</style>
