c8bcae2a32f0de709dd4e03aa31be523d498ff98
lukaskoeller <lukas-koeller@onlinehome.de>
2020-06-29 09:29:38 +0200

    Update src path/url for single posts as well

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -2,7 +2,7 @@
   <Layout>
     <article>
         <div class="article__hero">
-            <g-image class="article__img" :src="`http://localhost:1337${$page.strapiPost.image.url}`"/>
+            <g-image class="article__img" :src="$page.strapiPost.image.url"/>
         </div>
         <h1 class="article__title">{{ $page.strapiPost.title }}</h1>
         <div class="article__body">
